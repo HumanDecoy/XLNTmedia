9b78ab48fe1f76c88a262a6af6cb163c176b2480
Felix Fjellström <rayzoor@outlook.com>
2016-12-22 15:57:46 +0100

    EmilsHamburgare

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var hamburger = document.getElementById("nav-icon");
+hamburger.addEventListener("click", hamFunction);
+
+function hamFunction(){
+	document.getElementById("nav-icon").classList.toggle("open");
+};
+
