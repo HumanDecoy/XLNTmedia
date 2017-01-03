0438fb6bdb5389e9249da0c75e2f5cb40bb2014d
Oscar Gren <grenen89at-signgmail.com>
2017-01-03 14:56:25 +0100

    menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 var hamburger = document.getElementById("nav-icon");
+var box = document.getElementsByClassName("box");
+
 hamburger.addEventListener("click", hamFunction);
 
 function hamFunction(){
 	document.getElementById("nav-icon").classList.toggle("open");
-};
+}
+
+
+function buttonpress(){
+	box[0].classList.toggle("active");
+}
+
+
