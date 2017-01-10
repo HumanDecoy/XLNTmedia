339963c12b6983018ec1a009a47e1f340c70f5d2
Oscar Gren <Oscar Gren>
2017-01-10 13:26:56 +0100

    Fullaste hacket i världshistorien

--- js/main.js
+++ js/main.js
@@ -12,44 +12,85 @@ function hamFunction(){
 
 function buttonpress(){
 	box[0].classList.toggle("active");	
+
+	if (box[0].classList=="box active"){
+		menuFix();
+	}
+	else{
+		menuFix2();
+	}
 }
 
 var buttontrue=false;
 function notTrue(){
-    buttontrue=false;
+	buttontrue=false;
 }
 function buttonScroll(){
-    var button = document.getElementsByClassName("btn-1");
-    buttontrue=true;
+	var button = document.getElementsByClassName("btn-1");
+	buttontrue=true;
 }
 
 // Smooth scroll meny till target
 window.smoothScroll = function(target) {
-    var scrollContainer = target;
+	var scrollContainer = target;
     do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
+    	scrollContainer = scrollContainer.parentNode;
+    	if (!scrollContainer) return;
+    	scrollContainer.scrollTop += 1;
     } while (scrollContainer.scrollTop === 0);
 
     var targetY = 0;
     do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
+    	if (target == scrollContainer) break;
+    	targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
     scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    	i++; if (i > 30) return;
+    	c.scrollTop = a + (b - a) / 30 * i;
+    	setTimeout(function(){ scroll(c, a, b, i); }, 20);
 
     };
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
     if (buttontrue == false){
-        box[0].classList.toggle("active");
-        document.getElementById("nav-icon").classList.toggle("open");}
+    	box[0].classList.toggle("active");
+    	document.getElementById("nav-icon").classList.toggle("open");}
     };
+
+
+    function menuFix(){
+    	{
+    	setTimeout(function(){menuText[0].style.opacity="0";},130);
+    	setTimeout(function(){menuText[1].style.opacity="0";},130);
+    	setTimeout(function(){menuText[2].style.opacity="0";},130);
+    	setTimeout(function(){menuText[3].style.opacity="0";},130);
+    	setTimeout(function(){menuText[4].style.opacity="0";},130);
+    	setTimeout(function(){menuText[0].style.opacity="1"},400);
+    	setTimeout(function(){menuText[1].style.opacity="1"},400);
+    	setTimeout(function(){menuText[2].style.opacity="1"},400);
+    	setTimeout(function(){menuText[3].style.opacity="1"},400);
+    	setTimeout(function(){menuText[4].style.opacity="1"},400);
+    	}
+    	
+    }
+
+
+    function menuFix2(){
+    	{
+    	setTimeout(function(){menuText[0].style.opacity="0";},0);
+    	setTimeout(function(){menuText[1].style.opacity="0";},0);
+    	setTimeout(function(){menuText[2].style.opacity="0";},0);
+    	setTimeout(function(){menuText[3].style.opacity="0";},0);
+    	setTimeout(function(){menuText[4].style.opacity="0";},0);
+    	setTimeout(function(){menuText[0].style.opacity="1"},400);
+    	setTimeout(function(){menuText[1].style.opacity="1"},400);
+    	setTimeout(function(){menuText[2].style.opacity="1"},400);
+    	setTimeout(function(){menuText[3].style.opacity="1"},400);
+    	setTimeout(function(){menuText[4].style.opacity="1"},400);
+    	}
+    	
+    }
 
 
 
